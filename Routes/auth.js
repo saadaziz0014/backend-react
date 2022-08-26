68e19ce08db6e64aa041ea55c9da7eeb7b306245
saadaziz <saadaziz0014@gmail.com>
2022-08-26 22:30:29 +0500

    Harry Video 47

--- Routes/auth.js
+++ Routes/auth.js
@@ -2,28 +2,35 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const { findOne } = require("../models/User");
 
+//add data from api/auth/createuser
 router.post(
-  "/",
+  "/createuser",
   [
     body("email", "enter valid email").isEmail(),
     body("password", "password length must be 8").isLength({ min: 8 }),
     body("name", "name length must be 5").isLength({ min: 4 }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        res.status(400).json({ error: "User with this email already exist" });
       }
-      User.create({
+      user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-      }).then((user) => res.json(user));
+      });
+      res.json({ send: "Yes sent" });
     } catch (err) {
-      console.log(err.message);
-      res.status(500).send("Some error occured");
+      console.error(err.message);
+      res.status(400).send("Error occured");
     }
   }
 );
